c875e6beac812487f1c3a61dbc0ed39e93e84773
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2025-05-21 14:25:23 +0100

    health check update

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/health')
+def health_check():
+    return jsonify(status="ok"), 200
+
 @app.route('/data')
 def get_data():
     return jsonify(message="Yo this is tesa")
