717eb8501068b4bb6c00c73dddd1f06d9624a817
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2025-05-21 11:46:34 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/data')
+def get_data():
+    return jsonify(message="Yo this is tesa")
+
+@app.route('/user')
+def get_user():
+    return jsonify(message="We are the best cloud engineers in the world")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
